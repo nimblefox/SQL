d59eae0fc859215aa23f4c9cc983847bd7c453df
Farooq Sk <sk.farooq96406@gmail.com>
2021-06-27 02:06:59 +0530

    Add files via upload

--- SQL_3.sql
+++ SQL_3.sql
@@ -98,4 +98,49 @@ SELECT [CityName], COALESCE([LatestRecordedPopulation], '0') AS LatestRecordedPo
 so you will also find it in other RDBMSs, the other difference is 
 you can give a whole list of values to be checked to COALESCE whereas to ISNULL you can only pass one.*/
 
+/*Write a query using the Sales.SpecialOffer table. Display the difference 
+between the MinQty and MaxQty columns along with the SpecialOfferID
+and Description columns.*/
+SELECT [SpecialOfferID], [Description], [MaxQty], [MinQty], [MaxQty] - [MinQty] AS [Difference]
+	FROM [AdventureWorks2019].[Sales].[SpecialOffer]
 
+
+/*Write a query using the Sales.SpecialOffer table. Multiply the MinQty
+column by the DiscountPct column. Include the SpecialOfferID and 
+Description columns in the results.*/
+SELECT [SpecialOfferID], [Description], [MinQty], [DiscountPct], [MinQty]*[DiscountPct] AS [Multix]
+	FROM [AdventureWorks2019].[Sales].[SpecialOffer]
+
+
+-- Skipping easy ones
+
+
+/*Local temporary tables are the tables stored in tempdb.  
+These tables are automatically destroyed at the termination of the procedure or session. 
+They are specified with the prefix #, for example #table_name and these temp tables can be created with the same name in multiple windows.*/
+
+/*Global temporary tables are also stored in tempdb. They are available to all sessions and all users. 
+They are dropped automatically when the last session using the temporary table has completed. T
+hey are specified with the prefix ##, for example ##table_name.*/
+
+
+-- STring functions are very useful for data cleaning, checkout documentation
+-- RTRIM, LTRIM, and TRIM
+CREATE TABLE #trimExample (COL1 VARCHAR(10));
+GO
+--Populate the table
+INSERT INTO #trimExample (COL1)
+VALUES ('a'),('b   '),('   c'),('   d   ');
+--Select the values using the functions
+SELECT COL1, '*' + RTRIM(COL1) + '*' AS "RTRIM",
+ '*' + LTRIM(COL1) + '*' AS "LTRIM",
+ '*' + TRIM(COL1) + '*' AS "TRIM"
+FROM #trimExample;
+--Clean up
+DROP TABLE #trimExample;
+
+
+-- The LEFT and RIGHT functions return a specified number of characters on the left or right side of a string
+-- LEN and DATALENGTH 
+/*. DATALENGTH returns the same value as LEN when the string is a CHAR or VARCHAR data type, which takes one byte per character. 
+The difference occurs when using DATALENGTH on NCHAR or NVARCHAR data types, which take up to two bytes*/
