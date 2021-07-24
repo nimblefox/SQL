9f69fd70dfe93c93433a04badbded9fefd558d3e
Farooq Sk <sk.farooq96406@gmail.com>
2021-07-24 22:56:42 +0530

    Add files via upload

--- SQL_4.sql
+++ SQL_4.sql
@@ -27,3 +27,19 @@ INNER JOIN AdventureWorks2019.Sales.SalesOrderDetail d ON 1 = 1;
 SELECT c.CustomerID, c.PersonID, p.BusinessEntityID, p.LastName
 FROM AdventureWorks2019.Sales.Customer AS c
 INNER JOIN AdventureWorks2019.Person.Person AS p ON c.PersonID = p.BusinessEntityID;
+
+-- Joining on More Than One Column
+SELECT sod.SalesOrderID, sod.SalesOrderDetailID,
+ so.ProductID, so.SpecialOfferID, so.ModifiedDate
+FROM AdventureWorks2019.Sales.SalesOrderDetail AS sod
+INNER JOIN AdventureWorks2019.Sales.SpecialOfferProduct AS so
+ ON so.ProductID = sod.ProductID AND
+ so.SpecialOfferID = sod.SpecialOfferID
+WHERE sod.SalesOrderID IN (51116,51112);
+
+-- Joining Three or More Tables
+SELECT soh.SalesOrderID, soh.OrderDate, p.ProductID, p.Name
+FROM AdventureWorks2019.Sales.SalesOrderHeader as soh
+INNER JOIN AdventureWorks2019.Sales.SalesOrderDetail AS sod ON soh.SalesOrderID = sod.SalesOrderID
+INNER JOIN AdventureWorks2019.Production.Product AS p ON sod.ProductID = p.ProductID
+ORDER BY soh.SalesOrderID;
