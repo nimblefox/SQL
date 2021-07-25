e62cacb97c7e22f7467ab8f2cdf910cc32cb900a
Farooq Sk <sk.farooq96406@gmail.com>
2021-07-26 00:00:12 +0530

    Add files via upload

--- SQL_4.sql
+++ SQL_4.sql
@@ -43,3 +43,9 @@ FROM AdventureWorks2019.Sales.SalesOrderHeader as soh
 INNER JOIN AdventureWorks2019.Sales.SalesOrderDetail AS sod ON soh.SalesOrderID = sod.SalesOrderID
 INNER JOIN AdventureWorks2019.Production.Product AS p ON sod.ProductID = p.ProductID
 ORDER BY soh.SalesOrderID;
+
+SELECT soh.SalesOrderID, soh.OrderDate, p.ProductID, p.Name
+FROM Sales.SalesOrderHeader as soh
+INNER JOIN Sales.SalesOrderDetail AS sod ON soh.SalesOrderID = sod.SalesOrderID
+INNER JOIN Production.Product AS p ON sod.ProductID = p.ProductID
+ORDER BY soh.SalesOrderID;
